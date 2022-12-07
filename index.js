f12ff4c412f608e7a28e0a0672ccd0b4de8f049e
Mikael Trilles <mikaeltrilles@gmail.com>
2022-12-07 11:48:53 +0100

    Affiche d'une image sur la page / en ressource statique

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'home.html'));
 })
