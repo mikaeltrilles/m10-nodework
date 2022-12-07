33d35e0dbcfb575e536f445a0e2a9a51324f9c92
Mikael Trilles <mikaeltrilles@gmail.com>
2022-12-07 11:23:54 +0100

    routes / /about /contact en version fichier html

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("My first express app... Nothing special isn't it ?");
+  res.sendFile(path.resolve(__dirname, 'home.html'));
 })
 app.get('/about', (req, res) => {
-  res.json({
-    content: "About node in json format"
-  })
+  res.sendFile(path.resolve(__dirname, 'about.html'));
+})
+app.get('/about-json', (req, res) => {
+  res. json ({
+    content: "About page content !"
+  });
 })
 app.get('/contact', (req, res) => {
-  res.send("This is the contact page");
+  res.sendFile(path.resolve(__dirname, 'contact.html'));
 })
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
